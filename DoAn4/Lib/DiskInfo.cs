47e1c9f6d78d0cab2b40d16588a0da8076a9945b
Don Le <donleqt@gmail.com>
2016-05-20 23:05:44 +0700

    initial project

---
+++ DoAn4/Lib/DiskInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Partition_Manager
+{
+    class DiskInfo
+    {
+        public uint BytesPerSector;
+        public string diskID;
+        public ulong Size;
+
+        public DiskInfo(string diskID)
+        {
+            // TODO: Complete member initialization
+            this.diskID = diskID;
+        }
+    }
+}
---
+++ DoAn4/Lib/DiskSector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Partition_Manager
+{
+    class DiskSector
+    {
+        public byte[] Data;
+
+        public uint SectorSize { get; set; }
+
+        public long Offset { get; set; }
+    }
+}
